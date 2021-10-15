5ed3b2e3acb59288970e7ac1b4a86da9add91bc0
Claire Limesand <clairelimesand@gmial.com>
2021-10-15 15:53:38 -0500

    first pass

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,49 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+const maxItems = 5;
+
+function addItem(item){
+  basket.push(item)
+  return true;
+}
+//testing code
+console.log('basket: ', basket);
+console.log('adding broccoli to basket (expect true): ', addItem('broccoli'));
+console.log('basket: ', basket);
+
+function listItems(){
+  for ( let i = 0; i < basket.length; i++ ){
+    console.log (basket[i]);
+  }
+}
+//testing code
+console.log('In your shopping cart:');
+console.log(listItems());
+
+function empty(){
+  return basket = [];
+}
+//testing code
+console.log('removing items (expect empty array): ', empty());
+
+function isFull(){
+  if ( basket.length < maxItems ) {
+    return false;
+  } else {
+    return true;
+  }
+}
+//testing code
+console.log('number of items in basket: ', basket.length);
+console.log ('Checking if basket is full (expect false): ', isFull());
+
+console.log('adding broccoli to basket (expect true): ', addItem('broccoli'));
+console.log('adding tofu to basket (expext true): ', addItem('tofu'));
+console.log('adding eggs to basket (expect true): ', addItem('eggs'));
+console.log('adding yogurt to basket (expext true): ', addItem('yogurt'));
+console.log('adding bread to basket (expect true): ', addItem('bread'));
+
+console.log('number of items in basket: ', basket.length);
+console.log ('Checking if basket if full (expect true): ', isFull());
